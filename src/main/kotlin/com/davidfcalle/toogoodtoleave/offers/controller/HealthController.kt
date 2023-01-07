ae6e9e65548826552a1be517ba337455aa73c95e
David Calle <davidfcalle@gmail.com>
2023-01-07 19:09:16 +0100

    fix ebs setup

---
+++ src/main/kotlin/com/davidfcalle/toogoodtoleave/offers/controller/HealthController.kt
@@ -0,0 +1,12 @@
+package com.davidfcalle.toogoodtoleave.offers.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @GetMapping
+    fun health(): String {
+        return "healthy"
+    }
+}
